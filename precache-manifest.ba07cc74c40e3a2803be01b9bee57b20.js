bd8dc3e71465520b5a7a55a7a41a01dcb33b9901
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-11-02 16:19:03 -0600

    Updates

---
+++ precache-manifest.ba07cc74c40e3a2803be01b9bee57b20.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec5e2b2f9e01fdb4ee772c62c78722a",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "5edc4c7f6f95c4d4e71d",
+    "url": "/Portafolio/static/css/main.394618e9.chunk.css"
+  },
+  {
+    "revision": "6a1647844b85d8f7bbbf",
+    "url": "/Portafolio/static/js/2.e32a4e92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portafolio/static/js/2.e32a4e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5edc4c7f6f95c4d4e71d",
+    "url": "/Portafolio/static/js/main.63da9310.chunk.js"
+  },
+  {
+    "revision": "a5166c5545381b6e0a20",
+    "url": "/Portafolio/static/js/runtime-main.c0423103.js"
+  },
+  {
+    "revision": "e60b3bce1575da2ef212aeec441f4311",
+    "url": "/Portafolio/static/media/Nav.e60b3bce.jpg"
+  },
+  {
+    "revision": "ba4918de65eed4393b3235ad2ad7f0ed",
+    "url": "/Portafolio/static/media/Pet.ba4918de.JPG"
+  },
+  {
+    "revision": "f3d4e3f42921f4705dc11895ec7d1e71",
+    "url": "/Portafolio/static/media/Portafolio.f3d4e3f4.PNG"
+  },
+  {
+    "revision": "5cd53ca074d4a14992b6f95be2356867",
+    "url": "/Portafolio/static/media/Run-Buddy.5cd53ca0.JPG"
+  },
+  {
+    "revision": "cf5f33c111784a3c292db2508a32a868",
+    "url": "/Portafolio/static/media/ajax.cf5f33c1.pdf"
+  },
+  {
+    "revision": "87c690b201b0e377daf0dc487c435e05",
+    "url": "/Portafolio/static/media/background-light-grey.87c690b2.png"
+  },
+  {
+    "revision": "5a0033f1c13d32202e04f617eafdcc6e",
+    "url": "/Portafolio/static/media/gitDevJob.5a0033f1.PNG"
+  },
+  {
+    "revision": "1a7b8bca9219d0a334d9cde455ed53c3",
+    "url": "/Portafolio/static/media/nico.1a7b8bca.png"
+  },
+  {
+    "revision": "f916422eac6acece0a4af63441e4fea8",
+    "url": "/Portafolio/static/media/wordProjects.f916422e.png"
+  }
+]);
